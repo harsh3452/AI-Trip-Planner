b51336d87ce16a816d0a1d4d32c8f3bcd8946531
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2024-10-15 17:33:52 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,  // or any port you want
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
