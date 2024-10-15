1f65be1744021c85d53c5311a4d7c6677d578554
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2024-10-15 17:46:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateTrip from "./create-trip/index.jsx";
-import Header from "./components/custom/header.jsx";
+import Header from "./components/custom/Header.jsx";
 import { Toaster } from "./components/ui/sonner.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import ViewTrip from "./view-trip/[tripId]/index.jsx";
