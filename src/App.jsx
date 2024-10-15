22348359cf383e0b31847494995733857c6c607d
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2024-10-15 17:50:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
-import Hero from './components/custom/hero'
+import Hero from './components/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
